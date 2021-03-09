c656e1bdc0025332d5c5c7ed5648435b642c86c6
Jack <j.mcgirl2@nuigalway.ie>
2021-03-09 16:41:35 +0000

    Expanded some methods and added authentication token

--- BankingRMI_App/src/Account.java
+++ BankingRMI_App/src/Account.java
@@ -1,4 +1,18 @@
+import java.math.BigDecimal;
+import java.rmi.RemoteException;
 
 public class Account {
 
+	public String username;
+	public String password;
+	public int accountNum = 147258; // Random Start point
+	public BigDecimal balance;
+	
+	public Account(String Username, String password, BigDecimal openingBalance) throws RemoteException {
+		username = Username;
+		password = this.password;
+		openingBalance = balance;
+		accountNum = accountNum++;
+		accountNum++;
+	}
 }
--- BankingRMI_App/src/Bank.java
+++ BankingRMI_App/src/Bank.java
@@ -1,6 +1,8 @@
 import java.beans.Statement;
 import java.math.BigDecimal;
 import java.rmi.RemoteException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
@@ -11,23 +13,56 @@ public class Bank implements IBank {
 	{
 	}
 	
-	public void deposit(int account, BigDecimal amount) throws RemoteException, InvalidSession {
-	// implementation code
+	private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
+	private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
+
+	public static String generateNewToken() {
+	    byte[] randomBytes = new byte[24];
+	    secureRandom.nextBytes(randomBytes);
+	    return base64Encoder.encodeToString(randomBytes);
 	}
 	
-	public void withdraw(int account, BigDecimal amount) throws RemoteException, InvalidSession {
-	// implementation code
+	public long login(String username, String password) throws RemoteException, InvalidLogin {
+		try {
+			for (Account i:accounts)
+			{
+				if (i.username == username)
+				{
+					if (i.password == password)
+					{
+						return generateNewToken();
+					}
+					//throw new InvalidLogin();
+				}
+				throw new InvalidLogin();
+			}
+		}
+		catch (InvalidLogin IL){
+				System.out.println("Invalid Login for User: " +IL.getUsername());
+			}
 	}
-	
-	public BigDecimal getBalance(int account) throws RemoteException, InvalidSession {
-	// implementation code
+
+	public void deposit(int accountnum, BigDecimal amount, long sessionID) throws RemoteException, InvalidSession {
+		// TODO Auto-generated method stub
+		
 	}
-	
-	public Statement getStatement(Date from, Date to) throws RemoteException, InvalidSession {
-	// implementation code
+
+	public void withdraw(int accountnum, BigDecimal amount, long sessionID) throws RemoteException, InvalidSession {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public BigDecimal getBalance(int accountnum, long sessionID) throws RemoteException, InvalidSession {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException, InvalidSession {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	public static void main(String args[]) throws Exception {
-	// initialise Bank server - see sample code in the notes and online RMI tutorials for details
-	}
+		// initialise Bank server - see sample code in the notes and online RMI tutorials for details
+		}
 }
