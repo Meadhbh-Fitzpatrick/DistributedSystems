42d8bcf187dc1b1d37aaf3c934adfdc81b233411
Jack <j.mcgirl2@nuigalway.ie>
2021-03-09 17:00:58 +0000

    def

---
+++ BankingRMI_App/src/InvalidLogin.java
@@ -0,0 +1,10 @@
+
+public class InvalidLogin extends Exception {
+
+	private static final long serialVersionUID = -7599514687910701613L;
+	public String username;
+	
+	public String getUsername() {
+		return username;
+	}
+}
