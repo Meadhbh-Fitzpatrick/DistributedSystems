66f5ed122756101d13c0ede9176b8efe0560578f
Jack <j.mcgirl2@nuigalway.ie>
2021-03-09 15:46:39 +0000

    Reup

---
+++ BankingRMI_App/src/Statement.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.util.List;
+
+public class Statement implements IStatement {
+
+	private static final long serialVersionUID = -3393428484808591906L;
+
+	public int getAccountnum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Date getStartDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date getEndDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAccoutName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Transaction> getTransactions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
